3de0dbd789cbbeeb57260061f8bbc142635e302a
Arturas <99223804+arturasVis@users.noreply.github.com>
2023-11-02 22:01:10 +0000

    Fixed if 0 or less then 1

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let firstBoard=40
 createBoard(firstBoard,sketchBoard)
 button.addEventListener('click',()=>{
     let boardSize=prompt('Enter square size(100 max)')
-    if(boardSize<=100)
+    if(boardSize<=100 && boardSize >=1)
     {
         sketchBoard.innerHTML=''
         createBoard(boardSize,sketchBoard)
